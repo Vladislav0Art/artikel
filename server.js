8aad4e0c650ba563cdf55daa5abfc2a08a232bd7
TommyIQ <bmp110676@gmail.com>
2020-08-30 22:29:38 +0300

    Trying to deploy to Heroku

--- server.js
+++ server.js
@@ -56,6 +56,17 @@ mongoose.connect(mongoURI, {
   .catch(err => console.log(err));
 
 
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set a static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+  
 
 // starting server and listening to port
 const PORT = process.env.PORT || configKeys.PORT;
